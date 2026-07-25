a7abba122830652a6aea776ed689dd4ccba5d8b3
court0317 <vbyfjj8wpw@privaterelay.appleid.com>
2026-07-25 22:24:13 +1000

    Rooted Reboot 3-3-3 fixed

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE='rooted-cloud-v11-my-recipes';
 const ASSETS=[
   './',
-  './index.html?v=rooted330recipes',
-  './styles.css?v=rooted330recipes',
-  './navigation.js?v=rooted330recipes',
-  './app.js?v=rooted330recipes',
-  './v81.js?v=rooted330recipes',
+  './index.html?v=rooted331recipes',
+  './styles.css?v=rooted331recipes',
+  './navigation.js?v=rooted331recipes',
+  './app.js?v=rooted331recipes',
+  './v81.js?v=rooted331recipes',
   './manifest.json',
   './icon.svg',
   './icon-192.svg',
@@ -31,6 +31,6 @@ self.addEventListener('fetch',event=>{
         caches.open(CACHE).then(cache=>cache.put(event.request,copy));
         return response;
       })
-      .catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html?v=rooted330recipes')))
+      .catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html?v=rooted331recipes')))
   );
 });
