d0ad7cc7957a026095dab731138b37e7e7f7015d
court0317 <vbyfjj8wpw@privaterelay.appleid.com>
2026-07-26 10:07:38 +1000

    Flourish and Bloom Vol 3.5

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE='flourish-and-bloom-v3-4';
+const CACHE='flourish-and-bloom-v3-5';
 const ASSETS=[
   './',
-  './index.html?v=flourish340',
-  './styles.css?v=flourish340',
-  './navigation.js?v=flourish340',
-  './app.js?v=flourish340',
-  './flourish-bloom-recipes.js?v=flourish340',
-  './v81.js?v=flourish340',
+  './index.html?v=flourish350',
+  './styles.css?v=flourish350',
+  './navigation.js?v=flourish350',
+  './app.js?v=flourish350',
+  './flourish-bloom-recipes.js?v=flourish350',
+  './v81.js?v=flourish350',
   './manifest.json',
   './icon.svg',
   './icon-192.svg',
@@ -32,6 +32,6 @@ self.addEventListener('fetch',event=>{
         caches.open(CACHE).then(cache=>cache.put(event.request,copy));
         return response;
       })
-      .catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html?v=flourish340')))
+      .catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html?v=flourish350')))
   );
 });
